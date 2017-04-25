b45e9b1ced8279d817e2332ee4a0ab69ee6c7ba1
Chris Slowik <chris.slowik@gmail.com>
2017-04-25 10:12:15 -0500

    removed file access for chapter text, chapterview works

--- LeadershipKeys/Controllers/ChapterVC.swift
+++ LeadershipKeys/Controllers/ChapterVC.swift
@@ -13,18 +13,16 @@ class ChapterVC: UIViewController {
     var chapterIndex: Int
     var chapterTitle: String
     var chapterSubtitle: String
-    var chapterText: String
+    var chapterView: ChapterView! { return self.view as! ChapterView }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
     
-    init(index: Int, title: String, subtitle: String, text: String) {
+    init(index: Int, title: String, subtitle: String) {
         chapterIndex = index
         chapterTitle = title
         chapterSubtitle = subtitle
-        chapterText = text
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -32,8 +30,10 @@ class ChapterVC: UIViewController {
         chapterIndex = 0
         chapterTitle = "No chapter"
         chapterSubtitle = ""
-        chapterText = ""
         super.init(coder: aDecoder)
     }
     
+    override func loadView() {
+        view = ChapterView(frame: UIScreen.main.bounds, chapter: chapterIndex)
+    }
 }
--- LeadershipKeys/Views/ChapterView.swift
+++ LeadershipKeys/Views/ChapterView.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SnapKit
 
 class ChapterView: UIScrollView {
 
@@ -21,40 +22,79 @@ class ChapterView: UIScrollView {
 
     //MARK:- Chapter Layouts
     func layoutChapter(_ chapter: Int) {
+        // Make title
+        let titleLabel = UILabel()
+        titleLabel.font = UIFont.titleFont
+        titleLabel.textColor = UIColor.ebonyClay
+        
         switch chapter {
         case 0:
             //MARK:- Chapter 0
+            let title = "Emotional Intelligence Tools to go from Good to Great Leadership"
+            titleLabel.text = title
+            addSubview(titleLabel)
+            makeTitleConstraints(titleLabel)
             
             break
         case 1:
             //MARK:- Chapter 1
+            let title = "Leadership Two-Step"
+            
             break
         case 2:
             //MARK:- Chapter 2
+            let title = "Delegation"
+            
             break
         case 3:
             //MARK:- Chapter 3
+            let title = "Giving Feedback"
+            
             break
         case 4:
             //MARK:- Chapter 4
+            let title = "Coaching for Performance"
+            
             break
         case 5:
             //MARK:- Chapter 5
+            let title = "Assumption Ladder"
+            
             break
         case 6:
             //MARK:- Chapter 6
+            let title = "Assumption Ladder Applications"
+            
             break
         case 7:
             //MARK:- Chapter 7
+            let title = "Emotional Audit"
+            
             break
         case 8:
             //MARK:- Chapter 8
+            let title = "Tell, Sell, Test"
+            
             break
         case 9:
             //MARK:- Chapter 9
+            let title = "Managing Up & Partnering"
+            
+            break
+        case 10:
+            //MARK:- Chapter 10
+            let title = "White Space Issues"
+            
             break
         default:
             break
         }
+    }
+    
+    func makeTitleConstraints(_ titleLabel: UILabel) {
+        titleLabel.snp.makeConstraints({ (make) in
+            make.left.equalToSuperview().inset(24)
+            make.right.equalToSuperview().inset(24)
+        })
     }
 }
