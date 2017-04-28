49d60597c854f12ee231ef24a041d6c4fb59dd90
Chris Slowik <chris.slowik@gmail.com>
2017-04-28 09:39:24 -0500

    add chap7

--- LeadershipKeys/Views/ChapterView.swift
+++ LeadershipKeys/Views/ChapterView.swift
@@ -393,6 +393,23 @@ class ChapterView: UIScrollView {
             titleLabel.text = title
             addTitle(titleLabel)
             
+            let p1 = addParagraph(after: titleLabel, withText: "To understand your emotions and better manage them, use these questions. Use this audit when you feel yourself getting upset, frustrated, or your triggers got activated. Wait 5 seconds till you get an answer to each question.")
+            
+            let l1 = addList(after: p1,
+                             withItems: ["What am I thinking?",
+                                         "What am I feeling?",
+                                         "What do I want now?",
+                                         "How am I getting in the way?",
+                                         "What do I need to do differently now?"],
+                             listStyle: .number)
+            
+            let hr1 = addHorizontalRule(after: l1)
+            let leadershipHeading = addHeading(after: hr1, level: 2, withText: "Leadership Keys")
+            let leadershipList = addList(after: leadershipHeading,
+                                         withItems: ["<b>Tool:</b> Emotional Audit", "<b>Competency:</b> Self-Management"],
+                                         listStyle: .bullet)
+            _ = addQuote(after: leadershipList, withText: "\"Emotions are contagious and you are your most contagious one on your team as the leader. You are the “emotional thermostat” for the team. What are you spreading?\" <qi>Relly Nadler, Psy.D. 2011</qi>")
+            
             break
         case 8:
             //MARK:- Chapter 8
